f3f0d551dd8bc7125ae7335d5416876b4239e72d
Dalmir <dalmir@debian>
2014-05-18 02:58:09 -0700

    sync

--- Release/WeatherSensor/WeatherSensor.d
+++ Release/WeatherSensor/WeatherSensor.d
@@ -1,4 +1,4 @@
-WeatherSensor/WeatherSensor.d: ../WeatherSensor/WeatherSensor.cpp \
- ../WeatherSensor/WeatherSensor.h
+WeatherSensor/WeatherSensor.d WeatherSensor/WeatherSensor.o:  \
+ ../WeatherSensor/WeatherSensor.cpp ../WeatherSensor/WeatherSensor.h
 
 ../WeatherSensor/WeatherSensor.h:
